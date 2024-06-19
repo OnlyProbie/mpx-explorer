def17d473a899216d460e54a0a6a1a069217a724
wangzhan <wangzhanz@didiglobal.com>
2024-06-19 18:36:59 +0800

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mpx-explorer/',
   plugins: [
     vue(),
     nodePolyfills()
